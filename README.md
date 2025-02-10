b38689f7238c7985b2b4f7af11375790c7e0705a
Depilzone <sistemas@depilzone.net>
2025-02-10 16:13:26 -0500

    [ADD] (Entity_noRelationship, ProductDTO&Validation)

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
 
-1. Levantar la base de datos
-# Teslo API
-```docker-compose up -d```
+# E-Commerce Alpha API
+
+1. Clonar proyecto
+2. ```yarn install```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+6. Levantar: ```yarn start:dev``` 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ ],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [],
+    }),
+    ProductsModule
+  ],
 })
 export class AppModule {}
