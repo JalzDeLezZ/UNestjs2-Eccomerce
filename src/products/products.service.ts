29be31e97a871130fb93e7fa3c79819206df0774
Depilzone <sistemas@depilzone.net>
2025-02-11 08:49:33 -0500

    [REFACTOR] (Product entity and service) :: DB Create Product [ADD] (Error handling)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger(ProductsService.name);
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
   findAll() {
@@ -22,5 +43,15 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(
+        error.detail || 'There was an error creating the product',
+      );
+    }
+    this.logger.error(error.message);
+    throw new InternalServerErrorException('Error creating product');
   }
 }
