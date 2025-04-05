66c2950acc183d828a4b5c600e9cfe7ddc41df7a
jmc.master <j4ko.delez@gmail.com>
2025-04-04 20:25:51 -0500

    [UPDATE] (Files module) :: Enhance file upload functionality with custom file naming and size limits

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,7 +6,8 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
@@ -16,9 +17,20 @@ export class FilesController {
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
+      limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+      },
+      storage: diskStorage({
+        destination: './static/uploads',
+        filename: fileNamer,
+      }),
     }),
   )
   uploadProductImg(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new Error('File is empty');
+    }
+    console.log({imputFile: file});
     return {
       fileName: file.originalname,
     };
